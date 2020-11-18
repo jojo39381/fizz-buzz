a590bdda16b89d8852bda081d13ddca19a21295a
Joseph Yeh <josephycyeh@gmail.com>
2020-11-18 02:03:52 -0800

    answer

--- index.js
+++ index.js
@@ -12,3 +12,43 @@ const LaunchRequestHandler = {
     return startGame(handlerInput)
   }
 };
+
+
+
+  
+
+// start the game and set session attributes to save progress
+function startGame(handlerInput) {
+    const speechOutput = "Welcome to Fizz Buzz. We’ll each take turns counting up from one. However, you must replace numbers divisible by 3 with the word “fizz” and you must replace numbers divisible by 5 with the word “buzz”. If a number is divisible by both 3 and 5, you should instead say “fizz buzz”. If you get one wrong, you lose. ... OK, I’ll start... One."
+    // initialize game and set session attributes
+    const sessionAttributes = {}
+    Object.assign(sessionAttributes, {
+        lastNumber: 1,
+        currentNumber: 2,
+        correctAnswer: 2,
+        score: 0
+        
+      });
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+    return handlerInput.responseBuilder
+    .speak(speechOutput)
+    .reprompt(speechOutput)
+    .getResponse();
+
+}
+
+
+
+
+
+
+
+
+
+exports.handler = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    AnswerIntentHandler,
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
