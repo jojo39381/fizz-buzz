c27773bd6edc770be15cf6bce09cdef74251d1b9
Joseph Yeh <josephycyeh@gmail.com>
2020-11-18 01:55:02 -0800

    launch

---
+++ index.js
@@ -0,0 +1,14 @@
+const Alexa = require('ask-sdk-core');
+
+
+// handle launch request
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    
+
+    return startGame(handlerInput)
+  }
+};
