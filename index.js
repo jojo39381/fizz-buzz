e54994545cdb8765e3648efd6af7be908368a495
Joseph Yeh <josephycyeh@gmail.com>
2020-11-18 02:37:32 -0800

    fix stuff

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const LaunchRequestHandler = {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
-    
+    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
 
     return startGame(handlerInput)
   }
 };
-
 
 // handle intent request and check if it is an answer intent
 const AnswerIntentHandler = {
@@ -21,24 +20,19 @@ const AnswerIntentHandler = {
         && handlerInput.requestEnvelope.request.intent.name === 'AnswerIntent';
     },
     handle(handlerInput) {
-        return handlerUserAnswer(handlerInput)
+        handlerUserAnswer(handlerInput)
         
     }
   };
 
-  
+const speechOutput = "welcome to game"
 
-// start the game and set session attributes to save progress
 function startGame(handlerInput) {
-    const speechOutput = "Welcome to Fizz Buzz. We’ll each take turns counting up from one. However, you must replace numbers divisible by 3 with the word “fizz” and you must replace numbers divisible by 5 with the word “buzz”. If a number is divisible by both 3 and 5, you should instead say “fizz buzz”. If you get one wrong, you lose. ... OK, I’ll start... One."
-    // initialize game and set session attributes
     const sessionAttributes = {}
     Object.assign(sessionAttributes, {
-        lastNumber: 1,
         currentNumber: 2,
         correctAnswer: 2,
         score: 0
-        
       });
     handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
     return handlerInput.responseBuilder
@@ -48,32 +42,36 @@ function startGame(handlerInput) {
 
 }
 
-// handle user responses and determine if it is valid and correct
 function handlerUserAnswer(handlerInput) {
-    const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+    const sessionAttributes = attributesManager.getSessionAttributes();
     const currentNumber = sessionAttributes.currentNumber
     const { intent } = handlerInput.requestEnvelope.request
     const userAnswer = intent.slots.Answer.value
-    var correctAnswer = sessionAttributes.correctAnswer
-    const nextNumber = currentNumber + 1
-    const nextResponseNumber = currentNumber + 2
-    var speechOutput = ""
-    
+    const speechOutput = ""
+    if (userAnswer == correctAnswer) {
+        const sessionAttributes = {}
+        Object.assign(sessionAttributes, {
+            currentNumber: 2,
+            correctAnswer: 2,
+            score: 0
+        });
+        speechOutput = (currentNumber + 1).toString()
+
+    } else {
+        speechOutput = "I'm sorry, the correct response was . You lose! Thanks for playing Fizz Buzz. For another great Alexa game, check out Song Quiz"
+    }
+    return handlerInput.responseBuilder
+    .speak(speechOutput)
+    .reprompt(speechOutput)
+    .getResponse();
 
 }
 
-
-
-
-
-
-
-
-
 exports.handler = Alexa.SkillBuilders.custom()
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    AnswerIntentHandler,
-    )
-    .addErrorHandlers(ErrorHandler)
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        AnswerIntentHandler)
+    .addErrorHandlers(
+        )
     .lambda();
+
