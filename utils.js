a683d49a493ef8c1768d0744efde26bdfc2de7b3
Joseph Yeh <josephycyeh@gmail.com>
2020-11-18 13:10:02 -0800

    files

---
+++ utils.js
@@ -0,0 +1,85 @@
+// check if number should be changed to fizz, buzz, fizzbuzz, or stay the same, and return the change 
+module.exports.checkFizzBuzz = function checkFizzBuzz(number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        
+        return "fizz buzz"
+        
+    }
+    else if (number % 3 == 0) {
+        
+        correctAnswer = "fizz"
+       
+        return "fizz"
+    }
+    else if (number % 5 == 0) {
+        
+        correctAnswer = "buzz"
+        
+        return "buzz"
+    }
+    else {
+        
+        return number.toString()
+    }
+}
+
+
+
+
+
+
+
+// set session attributes to store game progress. lastNumber is the last number alexa said, currentNumber is the number the user will say, and correctAnswer is the right answer
+
+module.exports.setSessionAttributes = function setSessionAttributes(lastNumber, currentNumber, correctAnswer, handlerInput) {
+    const sessionAttributes = {}
+    Object.assign(sessionAttributes, {
+        lastNumber: lastNumber,
+        currentNumber: currentNumber,
+        correctAnswer: correctAnswer
+            
+    });
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
+}// check if number should be changed to fizz, buzz, fizzbuzz, or stay the same, and return the change 
+module.exports.checkFizzBuzz = function checkFizzBuzz(number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+        
+        return "fizz buzz"
+        
+    }
+    else if (number % 3 == 0) {
+        
+        correctAnswer = "fizz"
+       
+        return "fizz"
+    }
+    else if (number % 5 == 0) {
+        
+        correctAnswer = "buzz"
+        
+        return "buzz"
+    }
+    else {
+        
+        return number.toString()
+    }
+}
+
+
+
+
+
+
+
+// set session attributes to store game progress. lastNumber is the last number alexa said, currentNumber is the number the user will say, and correctAnswer is the right answer
+
+module.exports.setSessionAttributes = function setSessionAttributes(lastNumber, currentNumber, correctAnswer, handlerInput) {
+    const sessionAttributes = {}
+    Object.assign(sessionAttributes, {
+        lastNumber: lastNumber,
+        currentNumber: currentNumber,
+        correctAnswer: correctAnswer
+            
+    });
+    handlerInput.attributesManager.setSessionAttributes(sessionAttributes)
+}
